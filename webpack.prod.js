5a4fe6c798ebf5f8b60a71d32a0c5398f4773ae1
Carl Avidano <cavidano@gmail.com>
2021-06-29 05:10:45 -0400

    updated dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -29,7 +29,6 @@ module.exports = merge(common, {
                         plugins: [
                             "@babel/plugin-transform-shorthand-properties",
                             "@babel/plugin-proposal-object-rest-spread",
-                            "@babel/plugin-transform-runtime"
                         ]
                     }
                 }
