2ab7ea06f78b43cb75b38fdab3b1627b7ab9bed7
Carl Avidano <cavidano@gmail.com>
2021-07-01 05:22:04 -0400

    fixed babel runtime error

--- webpack.prod.js
+++ webpack.prod.js
@@ -29,6 +29,7 @@ module.exports = merge(common, {
                         plugins: [
                             "@babel/plugin-transform-shorthand-properties",
                             "@babel/plugin-proposal-object-rest-spread",
+                            "@babel/plugin-transform-runtime"
                         ]
                     }
                 }
